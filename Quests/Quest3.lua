106e4ecdc06bff4e7272d37dcf4fec700e7a1f8c
“[hiddenability]” <cutecoolth@gmail.com>
2017-07-22 08:43:05 +0700

    fix bug

--- Quests/Quest3.lua
+++ Quests/Quest3.lua
@@ -114,7 +114,7 @@ function Quest:leftovers()
 		return false
 	end
 	
-	if getMapName() == "Indigo Plateau Center Johto" or getMapName() == "Elite Four Will Room" or getMapName() == "Elite Four Koga Room" or getMapName() == "Elite Four Bruno Room Johto" or  getMapName() == "Elite Four Karen Room" or getMapName() == "Elite Four Champion Room Johto" or ( getMapName() == "Victory Road Kanto 3F" and getPokemonName(6) == "Rattata" ) then
+	if getMapName() == "Indigo Plateau Center Johto" or getMapName() == "Elite Four Will Room" or getMapName() == "Elite Four Koga Room" or getMapName() == "Elite Four Bruno Room Johto" or  getMapName() == "Elite Four Karen Room" or getMapName() == "Elite Four Champion Room Johto" or ( getMapName() == "Victory Road Kanto 3F" and getPokemonName(1) ~= "Rattata" ) then
 		if PokemonWithLeftovers > 0 then
 			takeItemFromPokemon(PokemonWithLeftovers)
 			return true
