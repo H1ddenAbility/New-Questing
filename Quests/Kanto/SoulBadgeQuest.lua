f3b7fc97a3e5ce16d3e26164cf840ef9335aaf03
DubsCheckum <m3rcury@tuta.io>
2017-09-06 00:40:14 +1200

    Update link name on Route 6 Stop House

--- Quests/Kanto/SoulBadgeQuest.lua
+++ Quests/Kanto/SoulBadgeQuest.lua
@@ -1,4 +1,4 @@
--- Copyright © 2016 g0ld <g0ld@tuta.io>
+-- Copyright Â© 2016 g0ld <g0ld@tuta.io>
 -- This work is free. You can redistribute it and/or modify it under the
 -- terms of the Do What The Fuck You Want To Public License, Version 2,
 -- as published by Sam Hocevar. See the COPYING file for more details.
@@ -247,7 +247,7 @@ end
 
 function SoulBadgeQuest:Route6StopHouse()
 	if hasItem("Bike Voucher") then
-		return moveToMap("Saffron City")
+		return moveToMap("Link") -- Saffron City
 	else 
 		return moveToMap("Route 6")
 	end
